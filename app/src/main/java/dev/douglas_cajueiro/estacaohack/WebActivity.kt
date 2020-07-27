8dc7bfa0d7b66f294f9e7296e593c004baa092b5
Douglas Cajueiro <dev_douglas_cajueiro@hotmail.com>
2020-07-27 09:40:46 -0300

    Configurada WebView

--- app/src/main/java/dev/douglas_cajueiro/estacaohack/WebActivity.kt
+++ app/src/main/java/dev/douglas_cajueiro/estacaohack/WebActivity.kt
@@ -2,10 +2,23 @@ package dev.douglas_cajueiro.estacaohack
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebViewClient
+import kotlinx.android.synthetic.main.activity_web.*
 
 class WebActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_web)
+
+        //Habilitar JavaScript
+        wbvSite.settings.javaScriptEnabled = true
+
+        //Abrir a URL do site do Cel.lep
+        wbvSite.loadUrl("http://br.cellep.com/estacaohack")
+
+        // Definir o cliente padrão para abertura de URLs
+        wbvSite.webViewClient = WebViewClient()
+
+
     }
 }
